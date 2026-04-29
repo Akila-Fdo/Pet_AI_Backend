f8da17ece3a9aa2f341c78621b0cddbe52b262e3
Akila Fernando <akilafernando196@gmail.com>
2026-04-29 12:42:37 +0530

    added new eye model

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DOG_SKIN_MODEL = os.path.join(BASE_DIR, "weights/dog_skin_model.pth")
-DOG_EYE_MODEL = os.path.join(BASE_DIR, "weights/dog_eye_model.pth")
+DOG_EYE_MODEL = os.path.join(BASE_DIR, "weights/dog_eye_model_ResNet_NEW.pth")
 CAT_SKIN_MODEL = os.path.join(BASE_DIR, "weights/cat_skin_model.pth")
 
 DEVICE = "cuda" if __import__("torch").cuda.is_available() else "cpu"
